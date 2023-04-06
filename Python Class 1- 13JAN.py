d270747914962d882d0dbc48e60c48c96ce3ea95
Karthik Raja <kkart@Torontino.com>
2023-04-06 15:44:09 -0600

    Trying to commit

---
+++ Python Class 1- 13JAN.py
@@ -0,0 +1,30 @@
+
+
+import keyword
+print("My name is Karthik Raja Kothandan")
+
+
+A="Thalapathy"
+print(A)
+
+printt="ad"
+print(printt)
+
+a=keyword.iskeyword("print")
+print(a)
+
+name="KAEfmfdirsd"
+print(name.upper())
+
+#typee=input("enter name : ")
+#print("hi "+ typee)
+
+#Num1= float(input("enter first number:"))
+#Num2= float(input("enter second number:"))
+#print (Num1+Num2)
+
+a,b,c=input("enter two numbers: ").split(":")
+print("value 1:" + a)
+print("value 2: "+ b)
+print('hi')
+
